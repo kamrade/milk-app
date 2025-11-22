656b7b51bdedbdc93039facb827537c3fd0cbd39
Dennis Michailov <denis.michailov@gmail.com>
2025-11-22 13:02:13 +0200

    TextInput Milk Showcase

---
+++ src/routes/showcase/text-input-milk-autofocus/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { TextInputMilk } from 'milk-lib';
+  import { Divider, PageTitle, Box } from '$lib';
+
+</script>
+
+<PageTitle>Text Input Milk with Autofocus</PageTitle>
+<Divider/>
+
+<Box>
+  <p>This TextInput should be focused by default</p>
+  <TextInputMilk autoFocus size="lg" placeholder="Text Input Milk Autofocus" />
+</Box>
